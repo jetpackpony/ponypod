2784e6fdaabcabb068494805a155c7b7a6a9274a
jetpackpony <jetpackpony@gmail.com>
2016-02-03 20:20:20 +0700

    Generated controllers and views; Added some step definitions

---
+++ app/controllers/episodes_controller.rb
@@ -0,0 +1,4 @@
+class EpisodesController < ApplicationController
+	def index
+	end
+end
--- features/step_definitions/list_steps.rb
+++ features/step_definitions/list_steps.rb
@@ -1,3 +1,7 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
 # Populate the podcasts table
 Given /the following podcasts exist/ do |podcasts_table|
@@ -22,16 +26,47 @@ When /^(?:|I )go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-Given /I click on "(.*?)"/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /I click on "(.*?)"/ do |link|
+    click_link(link)
 end
 
-Then /I should see "(.*?)"/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+	expect(page).to have_content(text)
+  # if page.respond_to? :should
+  #   page.should have_content(text)
+  # else
+  #   assert page.has_content?(text)
+  # end
 end
 
-Then /I should not see "(.*?)"/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+  expect(page).to have_xpath('//*', :text => regexp)
+  # if page.respond_to? :should
+  #   page.should have_xpath('//*', :text => regexp)
+  # else
+  #   assert page.has_xpath?('//*', :text => regexp)
+  # end
+end
+
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+	expect(page).to have_no_content(text)
+  # if page.respond_to? :should
+  #   page.should have_no_content(text)
+  # else
+  #   assert page.has_no_content?(text)
+  # end
+end
+
+Then /^(?:|I )should not see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+  expect(page).to have_no_xpath('//*', :text => regexp)
+
+  # if page.respond_to? :should
+  #   page.should have_no_xpath('//*', :text => regexp)
+  # else
+  #   assert page.has_no_xpath?('//*', :text => regexp)
+  # end
 end
 
 Then /I should see the audio player for "(.*?)"/ do |arg1|
