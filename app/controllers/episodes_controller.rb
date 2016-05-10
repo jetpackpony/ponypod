18137fe0c572a7f881702cb7e6fc791c085ab564
jetpackpony <jetpackpony@gmail.com>
2016-05-10 07:52:55 +0700

    Remove code that is not used
      So that we don't write useless tests
      - remove pages controller and spec
      - remove podcast new, edit, update, delete actions and specs
      - remove epiosdes indes, new, edit, update, delete actions and scpecs
      - remove routes for pages controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,64 +4,9 @@ class EpisodesController < ApplicationController
   before_action :set_is_viewed, only: [:show]
   before_action :set_is_subscribed, only: [:show]
 
-  # GET /episodes
-  # GET /episodes.json
-  def index
-    @episodes = Episode.all
-  end
-
   # GET /episodes/1
   # GET /episodes/1.json
   def show
-  end
-
-  # GET /episodes/new
-  def new
-    @episode = Episode.new
-  end
-
-  # GET /episodes/1/edit
-  def edit
-  end
-
-  # POST /episodes
-  # POST /episodes.json
-  def create
-    @episode = Episode.new(episode_params)
-
-    respond_to do |format|
-      if @episode.save
-        format.html { redirect_to @episode, notice: 'Episode was successfully created.' }
-        format.json { render :show, status: :created, location: @episode }
-      else
-        format.html { render :new }
-        format.json { render json: @episode.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /episodes/1
-  # PATCH/PUT /episodes/1.json
-  def update
-    respond_to do |format|
-      if @episode.update(episode_params)
-        format.html { redirect_to @episode, notice: 'Episode was successfully updated.' }
-        format.json { render :show, status: :ok, location: @episode }
-      else
-        format.html { render :edit }
-        format.json { render json: @episode.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /episodes/1
-  # DELETE /episodes/1.json
-  def destroy
-    @episode.destroy
-    respond_to do |format|
-      format.html { redirect_to episodes_url, notice: 'Episode was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   # POST /episodes/1/viewed_status
@@ -78,11 +23,6 @@ class EpisodesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_episode
       @episode = Episode.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def episode_params
-      params[:episode]
     end
 
     def viewed_param
