dacc7f57d7c2b89f66430e37334f1f06390ccbe4
jetpackpony <jetpackpony@gmail.com>
2016-05-16 09:30:25 +0700

    Add defaults to all the fields in the database
      So that nothing is ever null when pulled out of the database
      - make all foreign keys not_null
      - make all varchars empty strings

--- db/schema.rb
+++ db/schema.rb
@@ -11,39 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504073944) do
+ActiveRecord::Schema.define(version: 20160516021453) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "episodes", force: :cascade do |t|
-    t.string   "title",            limit: 255
-    t.integer  "podcast_id",       limit: 4
-    t.string   "mp3_link",         limit: 255
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "guid",             limit: 255
-    t.text     "full_description", limit: 65535
-    t.text     "summary",          limit: 65535
-    t.datetime "published_at"
+    t.string   "title",            limit: 255, default: ""
+    t.integer  "podcast_id",                                null: false
+    t.string   "mp3_link",         limit: 255, default: ""
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.string   "guid",             limit: 255, default: ""
+    t.text     "full_description",             default: ""
+    t.text     "summary",                      default: ""
+    t.datetime "published_at",                              null: false
   end
 
   add_index "episodes", ["guid"], name: "index_episodes_on_guid", unique: true, using: :btree
   add_index "episodes", ["podcast_id"], name: "index_episodes_on_podcast_id", using: :btree
 
   create_table "podcasts", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.string   "rss_link",    limit: 255
-    t.text     "description", limit: 65535
-    t.string   "image",       limit: 255
+    t.string   "title",       limit: 255, default: ""
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "rss_link",    limit: 255, default: ""
+    t.text     "description",             default: ""
+    t.string   "image",       limit: 255, default: ""
   end
 
   add_index "podcasts", ["rss_link"], name: "index_podcasts_on_rss_link", unique: true, using: :btree
 
   create_table "subscriptions", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.integer  "podcast_id", limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.integer  "user_id",    null: false
+    t.integer  "podcast_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "subscriptions", ["podcast_id"], name: "index_subscriptions_on_podcast_id", using: :btree
@@ -51,20 +54,20 @@ ActiveRecord::Schema.define(version: 20160504073944) do
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",       limit: 255, default: ""
+    t.string   "provider",   limit: 255, default: ""
+    t.string   "uid",        limit: 255, default: ""
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "viewed_episodes", force: :cascade do |t|
-    t.integer  "user_id",       limit: 4
-    t.integer  "episode_id",    limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "podcast_id_id", limit: 4
-    t.integer  "podcast_id",    limit: 4
+    t.integer  "user_id",       null: false
+    t.integer  "episode_id",    null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "podcast_id_id"
+    t.integer  "podcast_id"
   end
 
   add_index "viewed_episodes", ["episode_id"], name: "index_viewed_episodes_on_episode_id", using: :btree
