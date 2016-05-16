199e3296e6741bca2a9203f12bd0c3bce7b95b31
jetpackpony <jetpackpony@gmail.com>
2016-05-16 09:31:49 +0700

    Create test for podcast lising on home page

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User visits homepage" do
+  scenario "successfully" do
+    create :podcast, title: "Hello Internet"
+    create :podcast, title: "Stuff You Should Know"
+
+    visit root_path
+
+    expect(page).to have_css ".podcast h4", text: "Hello Internet"
+    expect(page).to have_css ".podcast h4", text: "Stuff You Should Know"
+  end
+end
