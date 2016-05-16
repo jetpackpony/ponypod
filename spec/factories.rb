1ad54a520ee0c46d869da43d32a8c7a6f3965023
jetpackpony <jetpackpony@gmail.com>
2016-05-16 11:14:09 +0700

    Add test for podcast show page
      - add class to episode titles
      - add class to podcast title
      - create episode factory
      - add test

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :podcast do
     title "Test podcast"
-    rss_link { "http://#{title}" }
+    sequence(:rss_link) { |n| "http://someaddress.com/#{n}" }
+  end
+
+  factory :episode do
+    podcast
+    title "Test episode"
+    sequence(:guid) { |n| "http://episode-#{n}" }
+    published_at Time.now
   end
 end
