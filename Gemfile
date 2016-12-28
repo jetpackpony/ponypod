991b70bdd99a4c41906b557665f097fec3ca38e1
jetpackpony <jetpackpony@gmail.com>
2016-12-28 15:10:39 +0300

    Add podcast syncronization code to model and tests

--- Gemfile
+++ Gemfile
@@ -18,6 +18,13 @@ gem 'puma', '~> 3.0'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
+# Use feedjira for RSS parsing
+gem 'feedjira'
+
+# Use sanitize gem for cleaning up HTML in the models
+gem "sanitize"
+gem "htmlentities"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -34,6 +41,11 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  # Stub http requests for tests
+  gem 'webmock'
 end
 
 ruby "2.2.3"
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,48 @@
 class Podcast < ApplicationRecord
   has_many :episodes, dependent: :destroy
+
+  # The feed syncronization stuff
+
+  def self.sync
+    Podcast.all.
+      map(&:syncronize).
+      select { |item| item[:errors].count > 0 }
+  end
+
+  def syncronize
+    @sync_result = { podcast_id: self.id, errors: [] }
+    feed = Feedjira::Feed.parse_with(Feedjira::Parser::ITunesRSS, Faraday.get(rss_link).body)
+    self.title = feed.title
+    self.description = feed.description
+    self.image = feed.itunes_image
+    feed.entries.each do |entry|
+      episode = self.episodes.find_or_create_by(guid: entry.entry_id) do |ep|
+        ep.published_at = Time.now
+      end
+      episode.title = entry.title
+      episode.mp3_link = entry.enclosure_url
+      episode.full_description = entry.summary
+      episode.summary = Podcast.extract_summary entry.summary
+      episode.published_at = DateTime.parse entry.published.to_s
+      begin
+        episode.save!
+      rescue Exception => e
+        @sync_result.errors.push "episode #{episode.guid}: #{e.message}"
+      end
+    end
+    begin
+      save!
+    rescue Exception => e
+      @sync_result.errors.push e
+    end
+    @sync_result
+  end
+
+  def self.extract_summary(summary)
+    Podcast.cleanup_text (summary.scan(/<p>(.+)<\/p>/).first || [summary]).first
+  end
+
+  def self.cleanup_text(text)
+    HTMLEntities.new.decode(Sanitize.fragment(text)).strip
+  end
 end
