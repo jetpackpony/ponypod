f21e40edf59aec15750334a610febb0bd1084ddc
jetpackpony <jetpackpony@gmail.com>
2016-12-28 12:40:32 +0300

    Remove not needed actions from episode controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,5 @@
 class EpisodesController < ApplicationController
-  before_action :set_episode, only: [:show, :update, :destroy]
+  before_action :set_episode, only: [:show]
 
   # GET /episodes
   def index
@@ -13,39 +13,9 @@ class EpisodesController < ApplicationController
     render json: @episode
   end
 
-  # POST /episodes
-  def create
-    @episode = Episode.new(episode_params)
-
-    if @episode.save
-      render json: @episode, status: :created, location: @episode
-    else
-      render json: @episode.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /episodes/1
-  def update
-    if @episode.update(episode_params)
-      render json: @episode
-    else
-      render json: @episode.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /episodes/1
-  def destroy
-    @episode.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_episode
       @episode = Episode.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def episode_params
-      params.fetch(:episode, {})
     end
 end
