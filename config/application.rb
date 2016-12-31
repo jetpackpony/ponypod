7af017b6106c5b6e9a7bcbdf7bc74933a6202919
jetpackpony <jetpackpony@gmail.com>
2017-01-01 01:16:32 +0300

    Setup CORS for all domains

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,14 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Setup CORS for everything
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
 
