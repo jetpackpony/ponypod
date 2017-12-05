c7907a7855c0f7b42185d2e3f83eb08d0de870f5
jetpackpony <jetpackpony@gmail.com>
2017-12-05 19:57:53 +0300

    Setup deployment to gcloud

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
