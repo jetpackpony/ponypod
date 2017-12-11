fae4026287d9c67eca44acf0d308140bbfc3070f
jetpackpony <jetpackpony@gmail.com>
2017-12-11 16:23:53 +0300

    Remove duplicate attribute from podcast model

--- models/podcast.js
+++ models/podcast.js
@@ -5,7 +5,6 @@ const Presenter = require('yayson')({adapter:'default'}).Presenter;
 
 const PodcastSchema = new mongoose.Schema({
   title: { type: String, default: '' },
-  title: { type: String, default: '' },
   image: { type: String, default: '' },
   summary: { type: String, default: '' },
   description: { type: String, default: '' },
