3e4ba0e22ba16923c89430804c8ddb00c546a2de
jetpackpony <jetpackpony@gmail.com>
2017-12-15 21:00:11 +0300

    Add actual feed parsing and tests

--- rssWorker/parseFeed.js
+++ rssWorker/parseFeed.js
@@ -1,34 +1,62 @@
 const R = require('ramda');
-const faker = require('faker');
+const striptags = require('striptags');
+const { parseIntDecimal } = require('../app/utils/numbers');
 
-const guids = R.times(R.identity, 2);
+const hmsToSeconds = ([h, m, s]) => h * 3600 + m * 60 + s;
+const nullsToZeros = (i) => i || 0;
+const nullsToEmptyStrings = (str) => str || "";
+
+const parseDuration =
+  R.compose(
+    hmsToSeconds,
+    R.map(nullsToZeros),
+    R.map(parseIntDecimal),
+    R.slice(1, 4),
+    R.match(/(?:(\d{1,2}):)?(\d{1,2}):(\d{1,2})/)
+  );
+
+const extractSummary =
+  (str) => (
+    R.compose(
+      striptags,
+      nullsToEmptyStrings,
+      (matches) => matches[1] || str,
+      R.match(/<p>(.+)<\/p>/),
+      R.invoker(0, 'trim')
+    )(str)
+  );
 
 const getPodcastDataFromFeed =
-  R.curry((podcast, feed) => ({
-    title: faker.company.companyName(),
-    image: faker.internet.avatar(),
-    summary: faker.lorem.paragraph(),
-    description: faker.lorem.paragraphs(),
+  R.curry((feed) => ({
+    title: feed.title,
+    image: feed.itunes.image,
+    summary: feed.description,
+    description: feed.description
   }));
 
 const getEpisodesDataFromFeed =
-  R.curry((podcast, feed) => guids.map((guid) => ({
-    podcast,
-    guid: `${podcast._id.toString()}_${guid + 1}`,
-    title: faker.company.companyName(),
-    publishedAt: faker.date.past(),
-    duration: faker.random.number(),
-    summary: faker.lorem.paragraph(),
-    fullDescription: faker.lorem.paragraphs(),
-    mp3Link: faker.internet.url()
-  })));
+  R.curry((podcast, feed) => (
+    feed.entries.map((ep) => ({
+      podcast,
+      guid: ep.guid,
+      title: ep.title,
+      publishedAt: ep.pubDate,
+      duration: parseDuration(ep.itunes.duration || '00:00'),
+      summary: extractSummary(ep.content),
+      fullDescription: ep.content,
+      mp3Link: ep.enclosure.url
+    }))
+  ));
 
 const parseFeed =
-  R.curry((podcast, feed) => ({
-    podcastData: getPodcastDataFromFeed(podcast, feed),
+  R.curry((podcast, { feed }) => ({
+    podcastData: getPodcastDataFromFeed(feed),
     episodesData: getEpisodesDataFromFeed(podcast, feed)
   }));
 
 module.exports = {
-  parseFeed
+  parseFeed,
+  parseDuration,
+  extractSummary
 };
+
---
+++ test/rssWorker/parseFeed.js
@@ -0,0 +1,84 @@
+process.env.NODE_ENV = 'test';
+
+const {
+  parseFeed,
+  parseDuration,
+  extractSummary
+} = require('../../rssWorker/parseFeed');
+const chai = require('chai');
+const expect = chai.expect;
+const Podcast = require('../../models/podcast').model;
+const testFeed = require('../testData/testFeed-hello.json');
+
+describe('parseFeed', () => {
+  let podcast;
+  beforeEach(() =>
+    Podcast
+    .create({ title: "test" })
+    .then((p) => podcast = p)
+  );
+  afterEach(() => Podcast.remove({}));
+
+  it('should return correct podcast data', () => {
+    expect(
+      parseFeed(podcast, testFeed).podcastData
+    ).to.be.eql({
+      title: "Hello Internet",
+      description: "CGP Grey and Brady Haran talk about YouTube, life, work, whatever.",
+      image: "http://static1.squarespace.com/Hello+Internet.003.png",
+      summary: "CGP Grey and Brady Haran talk about YouTube, life, work, whatever.",
+    });
+  });
+  it('should return correct episodes data', () => {
+    expect(
+      parseFeed(podcast, testFeed).episodesData
+    ).to.be.eql([
+      {
+        podcast,
+        guid: "52d66949e4b0a8cec3bcdd46",
+        title: "1-First episode",
+        publishedAt: "Thu, 30 Nov 2017 20:33:49 +0000",
+        duration: 6692,
+        summary: "First episode - First paragraph of description",
+        fullDescription: "\n\n\n\n<p>First episode - First paragraph of description</p>\n<h2 id=\"sponsors-\">Sponsors:</h2>\n<p><a href=\"https://www.squarespace.com/hello\">Some link</a></p>\n\n",
+        mp3Link: "http://hellointernet/HI_93_Export_4.mp3"
+      },
+      {
+        podcast,
+        guid: "52d66949e4b0a8cec3bcdd46",
+        title: "2-Second episode",
+        publishedAt: "Fri, 24 Nov 2017 22:20:12 +0000",
+        duration: 3222,
+        summary: "Second Episode - First paragrhaph of second episode",
+        fullDescription: "\n\n\n\n<p>Second Episode - First paragrhaph of <em>second</em> episode</p>\n<h2 id=\"sponsors-\">Sponsors:</h2>\n<p><a href=\"http://backblaze.com/hellointernet\">Some HTML</a></p>\n\n",
+        mp3Link: "http://hellointernet/HI92.mp3"
+      }
+    ]);
+  });
+});
+
+describe('parseDuration', () => {
+  it('parses hh:mm:ss duration', () => {
+    expect(parseDuration('01:54:21')).to.eql(6861);
+  });
+  it('parses mm:ss duration', () => {
+    expect(parseDuration('30:01')).to.eql(1801);
+  });
+  it('parses ss duration', () => {
+    expect(parseDuration('00:44')).to.eql(44);
+  });
+});
+
+describe('extractSummary', () => {
+  const withPar = "\n\n\n\n<p>Second Episode - First paragrhaph of <em>second</em> episode</p>\n<h2 id=\"sponsors-\">Sponsors:</h2>\n<p><a href=\"http://backblaze.com/hellointernet\">Some HTML</a></p>\n\n";
+  const firstPar = "Second Episode - First paragrhaph of second episode";
+  const withoutPar = "First paragraph Second episode of description stripped from HTML tags";
+
+  it('returns first paragraph if there is one', () => {
+    expect(extractSummary(withPar)).to.eql(firstPar);
+  });
+  it('returns the whole summary if there are no paragraphs', () => {
+    expect(extractSummary(withoutPar)).to.eql(withoutPar);
+  });
+});
+
